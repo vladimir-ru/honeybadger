510f5557851cc7b4e2d341c0180727d769033245
Maria <combichrist009official@gmail.com>
2023-04-06 21:09:26 +0300

    1 part main
    1 part of main page has been ended

--- HoneyBadger/main/urls.py
+++ HoneyBadger/main/urls.py
@@ -5,5 +5,6 @@ from django.conf import settings
 
 
 urlpatterns = [
-	path('', Main_Page.as_view(), name="main")
+	path('', Main_Page.as_view(), name="main"),
+    path('registration/', Reg_Page.as_view(), name="reg")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- HoneyBadger/main/views.py
+++ HoneyBadger/main/views.py
@@ -8,3 +8,11 @@ class Main_Page(View):
     def get(self, request):
         
         return render(request, 'pages/main.html', {})
+
+
+class Reg_Page(View):
+
+    def get(self, request):
+
+
+        return render(request, 'pages/reg.html', {})
